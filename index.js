b18bede78325bfaa6963cb0c63860e3d4b345a96
romankt21 <romankt21@gmail.com>
2023-05-19 22:37:05 +0300

    Co-authored-by: Yarema Ostrovskyi <Yarema91@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 let submit = document.forms.submit; // find element 
+const dateInput = document.getElementById("Date_trip");
+const selectStation = document.getElementById("Start_Station");
 
+
+
+
+const today = new Date().toISOString().split('T')[0];
+// console.log("today ", today );
+
+dateInput.setAttribute('min', today);
+
+// dateInput.setAttribute('max', today);
+
+// console.log("dateInput.setAttribute", dateInput.setAttribute);
 
 submit.addEventListener('submit', handleSubmit); //listener events
 
-const passenger = {
+const passengers = [
+    {
     id: 1324343434,
     firstName: "Roman",
     lasttName: "Kutnyi",
@@ -14,7 +28,23 @@ const passenger = {
     baggage: true,
     pet: false,
     exemption: ""
+},
+{
+    id: 1324343400,
+    firstName: "Roman",
+    lasttName: "Kutnyi",
+    start: "Lviv",
+    end: "Praga",
+    date: "21.06.23",
+    seatNumber: 46,
+    baggage: true,
+    pet: false,
+    exemption: ""
 }
+];
+
+
+const seatNumberFreeOnBus = [ 11, 32, 34, 45, 55 ]; //have green border
 
 // console.log("passenger", passenger);
 
@@ -41,7 +71,38 @@ function handleSubmit(event) {
 };
 
 
-const today = new Date();
-console.log("today ", today );
+const stationsDB =  [
+    {
+        city: "Lviv",
+        stations: {station: "BS-1", station: "BS-2 ", station: "BS-3"}, 
+        country: "Ukraine"
+    }, 
+    {
+        city: "Kyiv",
+        stations: {station: "BS-1", station: "BS-2", station: "BS-3", station: "BS-4", station: "BS-5", station: "BS-6"}, 
+        country: "Ukraine"
+    }, 
+    {
+        city: "Ternopil",
+        stations: {station: "BS-1", station: "BS-2", station: "BS-3"}, 
+        country: "Ukraine"
+    }, 
+    {
+        city: "Rome",
+        stations: {station: "Tiburtina (Stazione Tiburtina)", station: "Termini (Stazione Termini)", station: "EUR Fermi"}, 
+        country: "Italy"
+    }, 
+    
+];
+
+for(let station of stationsDB){
+    console.log(station.city);
+    const option = document.createElement('option')
+
+    option.value = station.city;
+option.textContent = `${station.city},${station.country} `;
+    selectStation.appendChild(option);
+}
+
 
 
